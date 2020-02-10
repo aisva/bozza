b3eec824947ebe35fb6c195601f05943740d79c0
aisva <aisasi@gmail.com>
2020-02-10 17:22:57 +0100

    Backend. Implemented the following Lambda function: getUser

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,6 +2,8 @@ export interface User {
   userId: string;
   username: string;
   password: string;
+  tags?: Array<string>;
+  receivers?: Array<string>;
   createdAt: string;
   updatedAt?: string;
 }
