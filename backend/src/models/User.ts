cfc06081120809ca7076ad9c31a1954d62b25009
aisva <aisasi@gmail.com>
2020-02-11 18:24:02 +0100

    Backend. Removed the following Lambda function: getUser

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,8 +2,6 @@ export interface User {
   userId: string;
   username: string;
   password: string;
-  tags?: Array<string>;
-  receivers?: Array<string>;
   createdAt: string;
   updatedAt?: string;
 }
