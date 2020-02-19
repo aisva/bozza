cb06a0e450bf15aa1eaf75e8ad65512b540eb1fc
aisva <aisasi@gmail.com>
2020-02-19 17:12:27 +0100

    Frontend. Added current date to log messages

--- frontend/src/utils/logger.js
+++ frontend/src/utils/logger.js
@@ -1,6 +1,6 @@
 const Logger = (entity, message, error = false) => {
   if (process.env.NODE_ENV == null || process.env.NODE_ENV === "development") {
-    const msg = `${entity} -> '${message}'`;
+    const msg = `[${new Date().toISOString()}] ${entity} -> '${message}'`;
     if (!error) {
       console.log(msg);
     } else {
